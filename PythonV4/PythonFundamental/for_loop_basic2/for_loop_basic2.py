af3b9ef5511ff8a7c8ee527bc0e003955c927632
“Rana” <eng.rana_zaro@hotmail.com>
2022-09-16 23:13:44 +0300

    edit

--- PythonV4/PythonFundamental/for_loop_basic2/for_loop_basic2.py
+++ PythonV4/PythonFundamental/for_loop_basic2/for_loop_basic2.py
@@ -56,55 +56,70 @@ length([37,2,1,-9])
 length([])
 
 
-def minimum(arr):
-    minimum=arr[0]
-       if len(arr)<1:
-       return False
- 
-   
-    for i in range(len(arr)):
-		    if minimum>arr[i]:
-		        minimum=arr[i]
+def minimum (list):
+   if len(list)>0:
+    min = list[0]
+    for i in range (len (list)):
+        if list[i]<min:
+            min= list[i]
+    return min
+   else:
+    return False
 
-    print(minimum)
+print(minimum([1,6,-9,8]))
+print(minimum([]))
+
 
 minimum([37,2,1,-9])
 minimum([])
 
 
-def maximum(arr):
-   max=arr[0]
-   
-    if len(arr)<1:
-		    return False
-    for i in range(len(arr)):
-        if max<arr[i]:
-        	max=arr[i]
-    print(max)
-maximum([37,2,1,-9])
-maximum([])
+def maximun (list):
+    if len(list)>0:
+        max = list[0]
+        for i in range (len(list)):
+            if list[i]>max:
+                max = list[i]
+        return max
+    else:
+        return False 
+print (maximun([37,2,1,-9]))
+print (maximun([]))
 
 
-def ultimate(arr):
-    min=arr[0]
-    max=arr[0]
-    total=0
-    i=0
-    for i in range(len(arr)):
-        if min > arr[i]:
-        	min=arr[i]
-        if max < arr[i]:
-        	max=arr[i]
-        total+=arr[i]
-    avg=total/len(arr)
-    length=len(arr)
-    print(min)
-    print(max)
-    print(total)
-    print(avg)
-    print(length)
+
+def ultimate(list):
+    min = list[0]
+    max = list[0]
+    sum = 0
+    avg = 0
+    for i in range (len(list)):
+        if list [i]<min:
+            min = list[i]
+        if list[i]>max:
+            max = list[i]
+        sum = sum + list[i]
+        avg = sum/len(list)
+        length = len(list)
+
+    print("minimum number is " + str(min))
+    print("maximum number is " + str (max))
+    print ("total value is " + str (sum))
+    print ("average number is " + str (avg))
+    print ("list length is " + str (length))
 
 ultimate([37,2,1,-9])
+
+
+
+def reverse (list):
+    for i in range (0, len(list)//2):
+        temp = list[i]
+        list[i] = list[len(list)-i-1]
+        list[len(list)-i-1]= temp
+    return list
+
+print (reverse([37,2,1,-9]))
 
 
 
