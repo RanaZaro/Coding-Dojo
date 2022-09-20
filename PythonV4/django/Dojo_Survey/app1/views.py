aacdb57840d338e8fe933dab22fbbc4d2a0b293a
“Rana” <eng.rana_zaro@hotmail.com>
2022-09-21 02:34:22 +0300

    dojo-survey

---
+++ PythonV4/django/Dojo_Survey/app1/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def index(request):
+    return render(request,'index.html')
+def result(request):
+    context = { 
+    'name':request.POST['name'],
+    'location':request.POST['location'],
+    'language':request.POST['language'],
+    'comment':request.POST['comment']
+    }
+    return render(request,'result.html',context)
+    
