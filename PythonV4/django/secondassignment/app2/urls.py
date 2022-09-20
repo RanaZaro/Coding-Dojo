ff5b8f4587cf65334a8f2285141f053876adac05
“Rana” <eng.rana_zaro@hotmail.com>
2022-09-20 23:51:14 +0300

    edit

--- PythonV4/django/secondassignment/app2/urls.py
+++ PythonV4/django/secondassignment/app2/urls.py
@@ -1,8 +1,5 @@
 from django.urls import path     
 from . import views
 urlpatterns = [
-    path('', views.index),	
-    path('hello', views.rana),	   
-]   
-
-
+    path('hi', views.index),	   
+]
