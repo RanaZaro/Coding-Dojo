5462d74f76089653ee7da3dcae4a1c4831a89c18
“Rana” <eng.rana_zaro@hotmail.com>
2022-09-28 14:58:51 +0300

    books_authors_proj

--- PythonV4/django/books_authors_proj/books_authors_app/urls.py
+++ PythonV4/django/books_authors_proj/books_authors_app/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
-     path('addbook', views.addbook),
+    path('addbook', views.addbook),
+    path('books/<int:id>', views.viewbook),
+    path('authtobook/<int:id>', views.authtobook),
 ]
--- PythonV4/django/books_authors_proj/books_authors_app/views.py
+++ PythonV4/django/books_authors_proj/books_authors_app/views.py
@@ -12,6 +12,21 @@ def addbook (request):
     Book.objects.create(title=request.POST['title'])
     return redirect ('/')
 
+def viewbook (request, id):
+    context = {
+        'book': Book.objects.get(id = int(id)),
+        'authors' : Author.objects.all()
+
+    }
+    return render (request,'viewbook.html',context)
+    
+
+def authtobook (request,id):
+    book1=Book.objects.get (id = int(id))
+    auth1 = Author.objects.get (id = request .POST['authtobook'])
+    book1.authors.add(auth1)
+    return redirect ('/books/'+ str(id))
+
 
 
 
